e4e1e2b5b5c47fb8dcfa741f10b2f4de69c11609
bhaskar <bhaskarj61@gmail.com>
2018-08-10 09:47:27 +0530

    fridayThird

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 export function getAuthServiceConfigs() {
   let config = new AuthServiceConfig(
@@ -44,7 +45,8 @@ const appRoutes: Routes = [
     AppComponent,
     ChatboxComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HeaderComponent
     
    
   ],
